bbde4abd44eea5ac78641f9432f03c76142c3d22
jaquelinedamian <2230219@aluno.univesp.br>
2025-09-25 18:43:39 -0300

    Ajustes Diversos

--- backend/Ambienta/settings.py
+++ backend/Ambienta/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from decouple import config 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-au^14bw5%)%--oz5=lqn4$go-_v^tm*i6y*xnjpca$a@v_q9b3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['192.168.15.3', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'Ambienta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ambienta_db',
-        'USER': 'ambienta',
-        'PASSWORD': 'ambienta',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
 
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.http import HttpRequest
 
-def login_view(request: HttpRequest):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+# --- Views que estavam causando o erro no template e URLs ---
 
-        user = authenticate(request, username=email, password=password)
+def register_view(request):
 
-        if user is not None:
-            login(request, user)
-            # Redireciona o usuário para a página do dashboard após o login
-            return redirect('dashboard_view')  
-        else:
-            return render(request, 'accounts/login.html', {'error_message': 'Credenciais inválidas. Por favor, tente novamente.'})
-    
-    return render(request, 'accounts/login.html')
+
+    return render(request, 'accounts/register.html', {}) 
+
+
+def login_view(request):
+
+    return render(request, 'accounts/login.html', {})
+
+
+def logout_view(request):
+
+    return redirect('home') 
