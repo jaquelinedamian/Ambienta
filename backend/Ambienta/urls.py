a918ef2d945716eb6d631f7f171842317826e9b3
jaquelinedamian <2230219@aluno.univesp.br>
2025-09-26 19:51:40 -0300

    Tirando a necessidade de login para paginas publicas

--- backend/Ambienta/urls.py
+++ backend/Ambienta/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('sensors/', include('sensors.urls')),
     path('accounts/', include('django.contrib.auth.urls')), 
