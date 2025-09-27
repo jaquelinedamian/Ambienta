4c52d42a6ed76141ce1239425000a5a8428aafd1
jaquelinedamian <2230219@aluno.univesp.br>
2025-09-27 00:40:13 -0300

    Tirando a necessidade de login para paginas publicas

--- backend/Ambienta/settings.py
+++ backend/Ambienta/settings.py
@@ -79,7 +79,8 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise logo após SecurityMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
