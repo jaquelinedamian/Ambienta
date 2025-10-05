b629a7c6959cc293bbb8db0a47d65b37be260af0
FNascim <fernansn5@gmail.com>
2025-10-04 23:12:36 -0300

    Add build script for Render deployment

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Instalar dependências Python
+cd backend
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Coletar arquivos estáticos
+python manage.py collectstatic --no-input
+
+# Aplicar migrações
+python manage.py migrate
